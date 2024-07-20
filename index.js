45001d3e18f34f3b3e74caf4d16659172b05e0e4
muruga21 <murugaperumal21062004@gmail.com>
2024-07-20 11:18:35 +0530

    connected to mongodb

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import typeDefs from './models/schema.js';
 import resolvers from './resolvers/resolver.js';
+import connectdb from './config/connectdb.js';
+import dotenv from 'dotenv';
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 })
+
+dotenv.config();
+
+connectdb();
 
 const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
